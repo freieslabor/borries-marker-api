de1c83a0521282c5a358a8aaa127e3f3d609e072
basti2342 <basti@randomprojects.de>
2015-04-22 18:43:55 +0200

    pep8ified

--- marker.py
+++ marker.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import serial, logging, time, threading, math
+import serial
+import logging
+import time
+import threading
 
 
-INIT = "*SQ;;*SQ;;*SQ;*CB;*INITstn;*INITzn;*DB;*CPa;*SE;*CPz;*CPd;*CPa;*SE;*INITp301;*OI;;*CPa;*RTEOF;*SE;*INITwn;*INITgn;*INITppn,pj;*INITzpj;*INITzn;*INITd16,0,5,500,20000;*INITdx15,0,30;*WD10;*WU10;*SE;*INITs100.00,100.00;*INITo-100,-100;;*INITno0,0;*INITrd+,+;*INITrr+,+;*INITrs+,+;LO1;*LBn;*INITesn,n;*INITze-,-;*VM6500,6500;*VN6500,6500;*VS400,400;*SE;*VB2200;*VH600,600;*VP600,600;*VC600,600;*SE;;*AC90000,90000;*LBd0.00;*LBm1.4,1.16;;*INITn1;;*INITxqrap;;*INITxp5;*INITzs0;;*INITaen,asn,adj;*MO18E1,9600;*CPa;*SE;;*INITbeL,0,beL,1,beL,2,beH,3,beL,4,beH,5,beH,6,beL,7,beL,8,beL,9,beL,10,beL,11,beL,12,beL,13,beL,14,beL,15,beL,16,beL,17,beL,18,beL,19,beL,20,beL,21,beL,22,beL,23,beL,24;*INITbaD,22,baD,21,baD,20,baD,19,baD,18,baD,17,baD,16,baD,15,baD,14,baD,13,baD,12,baD,11,baD,10,baD,9,baD,8,baD,7,baD,6,baD,5,baD,4,baD,3,baD,2,baD,1,ba0,0;*SE;;;;*VP1200,1200;;*CPa;*RTEOF;*SE;CS6;*INITbe0,5;;*CPa;*RTEOF;*SE;;*XRH;;*EB;*SE;;*SH;;*SH;;*SH;;*SH;;PU;;*INITrr+,+;"
+INIT = '*SQ;;*SQ;;*SQ;*CB;*INITstn;*INITzn;*DB;*CPa;*SE;*CPz;*CPd;*CPa;*SE;*INITp301;*OI;;*CPa;*RTEOF;*SE;*INITwn;*INITgn;*INITppn,pj;*INITzpj;*INITzn;*INITd16,0,5,500,20000;*INITdx15,0,30;*WD10;*WU10;*SE;*INITs100.00,100.00;*INITo-100,-100;;*INITno0,0;*INITrd+,+;*INITrr+,+;*INITrs+,+;LO1;*LBn;*INITesn,n;*INITze-,-;*VM6500,6500;*VN6500,6500;*VS400,400;*SE;*VB2200;*VH600,600;*VP600,600;*VC600,600;*SE;;*AC90000,90000;*LBd0.00;*LBm1.4,1.16;;*INITn1;;*INITxqrap;;*INITxp5;*INITzs0;;*INITaen,asn,adj;*MO18E1,9600;*CPa;*SE;;*INITbeL,0,beL,1,beL,2,beH,3,beL,4,beH,5,beH,6,beL,7,beL,8,beL,9,beL,10,beL,11,beL,12,beL,13,beL,14,beL,15,beL,16,beL,17,beL,18,beL,19,beL,20,beL,21,beL,22,beL,23,beL,24;*INITbaD,22,baD,21,baD,20,baD,19,baD,18,baD,17,baD,16,baD,15,baD,14,baD,13,baD,12,baD,11,baD,10,baD,9,baD,8,baD,7,baD,6,baD,5,baD,4,baD,3,baD,2,baD,1,ba0,0;*SE;;;;*VP1200,1200;;*CPa;*RTEOF;*SE;CS6;*INITbe0,5;;*CPa;*RTEOF;*SE;;*XRH;;*EB;*SE;;*SH;;*SH;;*SH;;*SH;;PU;;*INITrr+,+;'
 
 HOME = ';*INITrd+,+;*RX;*RY;*RTHOME;*SH;;*SE;;*OA;;*SH;;*SE;'
 
@@ -31,8 +34,9 @@ class Marker(threading.Thread):
     def __init__(self, dev, timeout=0):
         """Initializes marker and moves to home position."""
         threading.Thread.__init__(self)
-        logging.basicConfig(level=logging.DEBUG, format='%(asctime)s ' +
-            '%(levelname)-8s %(message)s', datefmt='%H:%M:%S.%f')
+        logging.basicConfig(level=logging.DEBUG,
+                            format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%H:%M:%S.%f')
         self.__serial = serial.Serial(dev, timeout=0)
         self.write_buf += INIT
         self.home()
@@ -43,9 +47,9 @@ class Marker(threading.Thread):
         while '\r' in self.read_buf:
             buf_split = self.read_buf.split('\r', 1)
             self.read_buf = buf_split[1]
-            #if buf_split[0].startswith('ST '):
-            #    self.answers.append(True)
-            #    print '%s executed' % self.commands[len(self.answers)-1]
+            # if buf_split[0].startswith('ST '):
+            #     self.answers.append(True)
+            #     print '%s executed' % self.commands[len(self.answers)-1]
             logging.debug('read: %s' % buf_split[0])
 
     def position(self):
@@ -116,6 +120,7 @@ class Marker(threading.Thread):
 
 
 if __name__ == '__main__':
+    import math
     m = Marker('/dev/ttyUSB0')
     points = [
             (100, 100),
@@ -127,8 +132,7 @@ if __name__ == '__main__':
     ]
     cycle_points = []
 
-    old_x, old_y = 0, 0
-    for i in xrange(250):
+    for i in xrange(50):
         angle = i*2*math.pi/50.0
         x = 50 + round(45 * math.cos(angle))
         y = 50 + round(45 * math.sin(angle))
