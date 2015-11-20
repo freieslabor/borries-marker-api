5d276b42cc47ce744773897da668492ff75d7a86
basti2342 <basti@randomprojects.de>
2015-11-20 19:43:05 +0100

    logging and time estimate

--- marker.py
+++ marker.py
@@ -5,10 +5,12 @@ import serial
 import logging
 import time
 import threading
+from datetime import datetime, timedelta
 
 
 INIT = '*SQ;;*SQ;;*SQ;*CB;*INITstn;*INITzn;*DB;*CPa;*SE;*CPz;*CPd;*CPa;*SE;*INITp301;*OI;;*CPa;*RTEOF;*SE;*INITwn;*INITgn;*INITppn,pj;*INITzpj;*INITzn;*INITd16,0,5,500,20000;*INITdx15,0,30;*WD10;*WU10;*SE;*INITs100.00,100.00;*INITo-100,-100;;*INITno0,0;*INITrd+,+;*INITrr+,+;*INITrs+,+;LO1;*LBn;*INITesn,n;*INITze-,-;*VM6500,6500;*VN%d,%d;*VS400,400;*SE;*VB%d;*VH600,600;*VP600,600;*VC600,600;*SE;;*AC90000,90000;*LBd0.00;*LBm1.4,1.16;;*INITn1;;*INITxqrap;;*INITxp5;*INITzs0;;*INITaen,asn,adj;*MO18E1,9600;*CPa;*SE;;*INITbeL,0,beL,1,beL,2,beH,3,beL,4,beH,5,beH,6,beL,7,beL,8,beL,9,beL,10,beL,11,beL,12,beL,13,beL,14,beL,15,beL,16,beL,17,beL,18,beL,19,beL,20,beL,21,beL,22,beL,23,beL,24;*INITbaD,22,baD,21,baD,20,baD,19,baD,18,baD,17,baD,16,baD,15,baD,14,baD,13,baD,12,baD,11,baD,10,baD,9,baD,8,baD,7,baD,6,baD,5,baD,4,baD,3,baD,2,baD,1,ba0,0;*SE;;;;*VP1200,1200;;*CPa;*RTEOF;*SE;CS6;*INITbe0,5;;*CPa;*RTEOF;*SE;;*XRH;;*EB;*SE;;*SH;;*SH;;*SH;;*SH;;PU;;*INITrr+,+;'
 HOME = ';*INITrd+,+;*RX;*RY;*RTHOME;*SH;;*SE;;*OA;;*SH;;*SE;'
+MOVE = ';*PR%02.2f,%02.2f;;*SH;*OA;*SE;'
 EMERGENCY_OFF = ';;*HE;;;'
 NEEDLE = 'SP1;;PD;*WT250;PU;*SE;'
 
@@ -31,12 +33,23 @@ class SerialAnswer(object):
     def done(self):
         return self.__done * self.multiplier
 
+    @property
+    def ready(self):
+        return self.done == self.tbd
+
+    @property
+    def perc_done(self):
+        return (self.done / float(self.tbd)) * 100
+
     def increment_done(self):
         self.__done += 1
 
+    def __str__(self):
+        return '%d/%d' % (self.done, self.tbd)
+
 
 class Marker(threading.Thread):
-    """Borries marker represenation."""
+    """Borries marker representation."""
     MAX_X = 122.5
     MAX_Y = 102.5
 
@@ -49,18 +62,18 @@ class Marker(threading.Thread):
     daemon = True
     running = True
 
-    # count<prefix of answer, SerialAnswer object>
-    count = {
-            'ST': SerialAnswer(.5),  # movement
-            }
-
-    def __init__(self, dev, slow_motion=False):
+    def __init__(self, device, slow_motion=False, log_level=logging.DEBUG):
         """Initializes marker and moves to home position."""
         threading.Thread.__init__(self)
-        logging.basicConfig(level=logging.INFO,
+        logging.basicConfig(level=log_level,
                             format='%(asctime)s %(levelname)-8s %(message)s',
                             datefmt='%H:%M:%S')
-        self.__serial = serial.Serial(dev, timeout=0)
+        self.__serial = serial.Serial(device, timeout=0)
+        # count<prefix of answer, SerialAnswer object>
+        self.count = {
+            'ST': SerialAnswer(.5),  # movement
+        }
+        self.start_time = datetime.now()
 
         if slow_motion:
             self.write_buf += INIT % (650, 650, 220)
@@ -83,8 +96,16 @@ class Marker(threading.Thread):
             if prefix in self.count:
                 count = self.count[prefix]
                 self.count[prefix].increment_done()
-                logging.info('%d/%d %s executed.'
-                             % (count.done, count.tbd, prefix))
+                runtime = (self.start_time - datetime.now()).seconds
+                eta = timedelta(seconds=(100 / count.perc_done) * runtime)
+
+                days, seconds = eta.days, eta.seconds
+                hrs = days * 24 + seconds // 3600
+                min = (seconds % 3600) // 60
+                sec = seconds % 60
+
+                logging.info('%s %s executed; %.2f%%; ETA: %02d:%02d:%02d.' %
+                             (count, prefix, count.perc_done, hrs, min, sec))
 
     def position(self):
         """Returns x and y position as tuple."""
@@ -103,7 +124,7 @@ class Marker(threading.Thread):
         self.__x = 0
         self.__y = 0
 
-    def emergency_off(self, cause='?'):
+    def emergency_off(self, cause='client'):
         """Sends emergency off sequence."""
         # make sure write buffer won't get send anymore
         self.running = False
