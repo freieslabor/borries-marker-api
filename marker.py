8c416992e38e7e79f37649932093483b6d3cb48b
basti2342 <basti@randomprojects.de>
2015-11-29 16:45:11 +0100

    Marker: preview introduced
    Signed-off-by: basti2342 <basti@randomprojects.de>

--- marker.py
+++ marker.py
@@ -7,6 +7,8 @@ import time
 import threading
 from datetime import datetime, timedelta
 import Image
+import ImageDraw
+import re
 
 
 INIT = '*SQ;;*SQ;;*SQ;*CB;*INITstn;*INITzn;*DB;*CPa;*SE;*CPz;*CPd;*CPa;*SE;*INITp301;*OI;;*CPa;*RTEOF;*SE;*INITwn;*INITgn;*INITppn,pj;*INITzpj;*INITzn;*INITd16,0,5,500,20000;*INITdx15,0,30;*WD10;*WU10;*SE;*INITs100.00,100.00;*INITo-100,-100;;*INITno0,0;*INITrd+,+;*INITrr+,+;*INITrs+,+;LO1;*LBn;*INITesn,n;*INITze-,-;*VM6500,6500;*VN%d,%d;*VS400,400;*SE;*VB%d;*VH600,600;*VP600,600;*VC600,600;*SE;;*AC90000,90000;*LBd0.00;*LBm1.4,1.16;;*INITn1;;*INITxqrap;;*INITxp5;*INITzs0;;*INITaen,asn,adj;*MO18E1,9600;*CPa;*SE;;*INITbeL,0,beL,1,beL,2,beH,3,beL,4,beH,5,beH,6,beL,7,beL,8,beL,9,beL,10,beL,11,beL,12,beL,13,beL,14,beL,15,beL,16,beL,17,beL,18,beL,19,beL,20,beL,21,beL,22,beL,23,beL,24;*INITbaD,22,baD,21,baD,20,baD,19,baD,18,baD,17,baD,16,baD,15,baD,14,baD,13,baD,12,baD,11,baD,10,baD,9,baD,8,baD,7,baD,6,baD,5,baD,4,baD,3,baD,2,baD,1,ba0,0;*SE;;;;*VP1200,1200;;*CPa;*RTEOF;*SE;CS6;*INITbe0,5;;*CPa;*RTEOF;*SE;;*XRH;;*EB;*SE;;*SH;;*SH;;*SH;;*SH;;PU;;*INITrr+,+;'
@@ -63,13 +65,16 @@ class Marker(threading.Thread):
     daemon = True
     running = True
 
-    def __init__(self, device, slow_motion=False, log_level=logging.DEBUG):
+    def __init__(self, device, slow_motion=False, initial_check=False,
+        log_level=logging.DEBUG):
+
         """Initializes marker and moves to home position."""
         threading.Thread.__init__(self)
         logging.basicConfig(level=log_level,
                             format='%(asctime)s %(levelname)-8s %(message)s',
                             datefmt='%H:%M:%S')
         self.__serial = serial.Serial(device, timeout=0)
+        self.checked = not initial_check
         # count<prefix of answer, SerialAnswer object>
         self.count = {
             'ST': SerialAnswer(.5),  # movement
@@ -139,6 +144,8 @@ class Marker(threading.Thread):
     def move_rel(self, x, y):
         """Moves to given relative position."""
         if 0 <= x + self.__x <= self.MAX_X and 0 <= y + self.__y <= self.MAX_Y:
+            x = round(x, 2)
+            y = round(y, 2)
             self.write_buf += r';*PR%02.2f,%02.2f;;*SH;*OA;*SE;' % (x, y)
 
             self.__x = self.__x + x
@@ -167,6 +174,11 @@ class Marker(threading.Thread):
         width = end_x - start_x
         height = end_y - start_y
         img = Image.open(image_file)
+
+        if img.size[0] < width*granularity or img.size[1] < height*granularity:
+            self.emergency_off('Image resolution is too small for given '
+                               'bounding box and granularity.')
+
         img = img.resize((width*granularity, height*granularity),
                          Image.ANTIALIAS)
         granularity = float(granularity)
@@ -176,11 +188,59 @@ class Marker(threading.Thread):
                 pixel_value = img_bw.getpixel((x, y))
                 if pixel_value == 0:
                     self.move_abs(start_x+(x*(1/granularity)),
-                                  start_y+(y*(1/granularity))
+                                  start_y+(y*(1/granularity)))
                     self.needle_down()
+
+    def check(self, rds=10, preview_file='preview.png'):
+        start = datetime.now()
+
+        white = (255, 255, 255)
+        black = (0, 0, 0)
+
+        im = Image.new('RGB', (int(self.MAX_X*100), int(self.MAX_Y*100)),
+                       white)
+        draw = ImageDraw.Draw(im)
+
+        move_re = re.escape(MOVE).replace('\\%02\\.2f', '(\-?\d+\.\d\d)')
+        move_re_cmpld = re.compile(move_re)
+        x, y = (0, 0)
+        pos = 0
+
+        for n in re.finditer(re.escape(NEEDLE), self.write_buf):
+            for move in move_re_cmpld.findall(self.write_buf[pos:n.start()]):
+                x_rel, y_rel = move
+                x += float(x_rel)
+                y += float(y_rel)
+
+                x = round(x, 2)
+                y = round(y, 2)
+
+                assert 0 <= x <= self.MAX_X, 'x value %02.2f not in range.' % x
+                assert 0 <= y <= self.MAX_Y, 'y value %02.2f not in range.' % y
+
+            pos = n.start()
+            draw.ellipse((x*100-rds, y*100-rds, x*100+rds, y*100+rds), black)
+
+        im.save(preview_file)
+
+        # preview check by user
+        logging.info('Preview saved as %s (took %s). Please check it.'
+                     % (preview_file, datetime.now() - start))
+
+        cont = ' '
+        while cont.lower() not in ['y', 'n']:
+            cont = raw_input('Start marking? [y/n]')
+
+        if cont.lower() == 'n':
+            quit()
+
+        self.checked = True
 
     def run(self):
         """Thread loop."""
+        while not self.checked:
+            pass
+
         while self.running:
             if ';;' not in self.write_buf:
                 self.write_buf += ';*SH;;*SH;'
@@ -196,10 +256,13 @@ class Marker(threading.Thread):
 
 
 if __name__ == '__main__':
-    m = Marker('/dev/ttyUSB1')
+    m = Marker('/dev/ttyUSB1', initial_check=True)
     m.start()
 
     m.mark_picture('Logo_quadratisch.png', (0, 0, 50, 50))
+
+    # check
+    m.check()
 
     # move to home position for next session
     m.home()
